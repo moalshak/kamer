a985d8678b554a81292f8ddca40afc9b8fb943d6
Mohammad <mo.alshakoush@gmail.com>
2022-01-14 23:44:13 +0100

    more design changes

--- kamer/react-frontend/src/App.js
+++ kamer/react-frontend/src/App.js
@@ -1,23 +1,21 @@
 import axios from "axios";
 import React from "react";
 import ControlPanel from './Components/ControlPanel';
-import Property, {getProperties, getPropertyById} from './Components/Property';
+import Property, {getProperties} from './Components/Property';
 import {useState} from "react";
 import InputPanel from './Components/InputPanel';
-
-import * as prop from './Components/Property';
-
 // import PropertyFeed from './Components/PropertyFeed';
 // import InputPanel from './Components/InputPanel';
+
+const BASE_URL = "https://www.team13.xyz/api/";
 
 function App() {
 
     axios.defaults.headers.common['Authorization'] = 'Token ff27fca94c0c9461da6e327389e7b633224cd2fa'
 
     //properties state
+    // `getProperties` will "watch" the `currUrl` and update whenever it (currUrl) changes
     const [properties, setProperties] = useState([])
-
-
     /* state for the navigation */
     const [nav, setNav] = useState(true);
 
@@ -27,12 +25,11 @@ function App() {
     const [cityForm, setCityForm] = useState(false)
     const [showForm, setShowForm] = useState('')
 
-
-
+    /***/
     const onControlPanelClick = async (form) => {
         /* the form is undefined whenever the `search by all` is clicked */
         if (form === undefined) {
-            const data = await prop.getProperties('https://www.team13.xyz/api/all/?format=json&page=1');
+            const {data, next, prev} = await getProperties(`${BASE_URL}all/?format=json&page=1`);
             setProperties(data);
             setNav(true);
         } else {
@@ -44,7 +41,7 @@ function App() {
      * search by id is clicked
      * */
     const onIdGet = async (id) => {
-        const data = await prop.getPropertyById(id);
+        const {data, next, prev} = await getProperties(`${BASE_URL}id/${id}/?format=json`);
         setProperties(data);
         setNav(false);
     }
@@ -55,7 +52,10 @@ function App() {
             <ControlPanel 
             onButtonClick={onControlPanelClick}
                 />
-            <Property properties={properties} setProperties={setProperties} nav={nav} setNav={setNav}/>
+            <Property
+                properties={properties} setProperties={setProperties}
+                nav={nav} setNav={setNav}
+            />
             <InputPanel
             formToShow={showForm}
             onIdGet={onIdGet}
@@ -65,5 +65,6 @@ function App() {
         </div>
     );
 }
+
 
 export default App;
--- kamer/react-frontend/src/Components/Property.js
+++ kamer/react-frontend/src/Components/Property.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import {useEffect, useState} from "react";
 import MapBuilder from './MapBuilder';
 
+
+const BASE_URL = "https://www.team13.xyz/api/";
+
 /**
  * Calls the API through a get method
  * 
@@ -13,17 +16,16 @@ function Property ({ properties, setProperties, nav, setNav}) {
     const [loading, setLoading] = useState(true);
     
     /* state for the url */
-    const [currUrl, setCurrUrl] = useState('https://www.team13.xyz/api/all/?format=json&page=1')
+    const [currUrl, setCurrUrl] = useState(`${BASE_URL}all/?format=json&page=1`)
     const [nextUrl, setNextUrl] = useState(null)
     const [prevUrl, setPrevUrl] = useState(null)
     
     /* state for the current page number the user is on */
     const [curPage, setCurPage] = useState(1);
 
-
-
     // `getProperties` will "watch" the `currUrl` and update whenever it (currUrl) changes
     useEffect(() => {
+        setLoading(true);
         getProperties();
     }, [currUrl])
 
@@ -82,55 +84,83 @@ function Property ({ properties, setProperties, nav, setNav}) {
         }   
     }
 
+    /**
+     * Navigation component
+     * */
+    function Navigation () {
+        if (!nav) {
+            return null;
+        }
+        return (
+            <div className="nav">
+                    <small>Current Page: {getPageNumber('current')} </small>
+                    <br/>
+                    <small>Next Page: {getPageNumber('next')} </small>
+                    <br/>
+                    <small>Previous Page: {getPageNumber('prev')} </small>
+                    <div className="NextPrevBtn">
+                        <button onClick={prevPage}>Previous Page</button>
+                        <button onClick={nextPage}>Next Page</button>
+                    </div>
+            </div>
+        );
+    }
+
+    /**
+     * list of the description of the properties
+     * */
+    function Desc() {
+        return (
+            properties.map((prop) => {
+                return (
+                    <div className='propDiv'>
+                            <li key={prop.externalId}><h2>{prop.externalId}</h2></li>
+                            <ul>
+                            <li><h3>{prop.title}</h3></li>
+                            <li> <img src={prop.coverImageUrl}/></li>
+                            <li>AreaSqm: {prop.areaSqm} </li>
+                            <li>City: {prop.city} </li>
+                            <li>Furnish: {prop.furnish} </li>
+                            <li>Latitude: {prop.latitude} </li>
+                            <li>Longitude: {prop.longitude} </li>
+                            <li>PostalCode: {prop.postalCode} </li>
+                            <li>PropertyType: {prop.propertyType} </li>
+                            <li>Rent: {prop.rent} </li>
+                            <li>AdditionalCost: {prop.additionalCost} </li>
+                            <li>Deposit: {prop.deposit} </li>
+                            <li>DescriptionTranslated: {prop.descriptionTranslated} </li>
+                            <li>Gender: {prop.gender} </li>
+                            <li>IsRoomActive: {prop.isRoomActive} </li>
+                            <li>PageDescription: {prop.pageDescription} </li>
+                            <li>PageTitle: {prop.pageTitle} </li>
+                            <li>Pets: {prop.pets} </li>
+                            <li>Roommates: {prop.roommates} </li>
+                                {/* FIXME: NOTE::: I have disabled the maps since at sometimes the Google api will refuse to give
+                                    a response for some reason and that would crash the whole UI... maybe a genuine api will fix it ?
+                                I got this error :
+                                There has been an Error with loading Google Maps API script, please check that you provided correct google API key (AIzaSyBppjwBEh_iphL_o7XPFaVtRq02tL5Gzfc) or Client ID (-) to <LoadScript />
+                                Otherwise it is a Network issue.
+                                */}
+                        {/*<MapBuilder lng = {prop.longitude} lat={prop.latitude}/>*/}
+
+                            </ul>
+                    </div>
+                );
+            })
+        );
+    }
 
     return (
         <div className="container">
 
             <h3>DATA</h3>
 
-            {nav ?             <div className="nav">
-                <small>Current Page: {getPageNumber('current')} </small>
-                <br/>
-                <small>Next Page: {getPageNumber('next')} </small>
-                <br/>
-                <small>Previous Page: {getPageNumber('prev')} </small>
-                <div className="NextPrevBtn">
-                    <button onClick={prevPage}>Previous Page</button>
-                    <button onClick={nextPage}>Next Page</button>
-                </div>
-            </div> : null}
-            
-            {loading ? <b>"loading.."</b> : null} {/* if loading view loading */}
+            <Navigation />
+
+            {loading ? <h1>"loading.."</h1> : null} {/* if loading view loading */}
+
             <ul>
-                {properties.map((prop) => {
-                    return (
-                    <div className='propDiv'>
-                        <li key={prop.externalId}><h2>{prop.externalId}</h2></li>
-                        <ul>
-                        <li><h3>{prop.title}</h3></li>
-                        <li> <img src={prop.coverImageUrl}/></li>
-                        <li>AreaSqm: {prop.areaSqm} </li>
-                        <li>City: {prop.city} </li>
-                        <li>Furnish: {prop.furnish} </li>
-                        <li>Latitude: {prop.latitude} </li>
-                        <li>Longitude: {prop.longitude} </li>
-                        <li>PostalCode: {prop.postalCode} </li>
-                        <li>PropertyType: {prop.propertyType} </li>
-                        <li>Rent: {prop.rent} </li>
-                        <li>AdditionalCost: {prop.additionalCost} </li>
-                        <li>Deposit: {prop.deposit} </li>
-                        <li>DescriptionTranslated: {prop.descriptionTranslated} </li>
-                        <li>Gender: {prop.gender} </li>
-                        <li>IsRoomActive: {prop.isRoomActive} </li>
-                        <li>PageDescription: {prop.pageDescription} </li>
-                        <li>PageTitle: {prop.pageTitle} </li>
-                        <li>Pets: {prop.pets} </li>
-                        <li>Roommates: {prop.roommates} </li>
-                        <MapBuilder lng = {prop.longitude} lat={prop.latitude}/>
-                        </ul>
-                    </div>
-                    );
-                })}
+                <Desc/>
             </ul>
         </div>
     )
@@ -138,14 +168,31 @@ function Property ({ properties, setProperties, nav, setNav}) {
 }
 
 /**
- * fetched the properties from the api call
+ * fetches the properties from the api at a given url
+ *
+ * @param url the url to get the properties of
+ * @return object that has the data, next and previous pages urls
  */
 export const getProperties = async (url) => {
     try {
         const response = await axios.get(url);
-        const next = await response.data.next;
-        const prev = await response.data.previous;
-        return await response.data.results;
+        let next = null;
+        let prev = null;
+        let results = await response.data.results;
+        /* whenever the results is undefined that means there was no pagination
+        * available on tha page so get the data from `response.data`
+        * */
+        if (results === undefined) {
+            results = [response.data];
+        } else {
+            next = await response.data.next;
+            prev = await response.data.previous;
+        }
+        return ({
+            data: results,
+            next: next,
+            prev: prev,
+        });
     } catch(error) {
         console.log(error.response);
         console.log(error.response.status);
@@ -153,21 +200,5 @@ export const getProperties = async (url) => {
         return null;
     }
 }
-
-export const getPropertyById = async (id) => {
-    const baseURL = "https://www.team13.xyz/api/";
-
-    try {
-        const response = await axios.get(baseURL + "id/" + id + "/?format=json");
-        const result = await response.data;
-        return [result];
-    } catch (error) {
-        console.log(error.response);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        return null;
-    }
-}
-
 
 export default Property
