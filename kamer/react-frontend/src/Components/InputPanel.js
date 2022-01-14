111bd64483a493f2774a445aa317878866ba2e25
Mohammad <mo.alshakoush@gmail.com>
2022-01-14 12:20:32 +0100

    updated css

--- kamer/react-frontend/src/Components/InputPanel.js
+++ kamer/react-frontend/src/Components/InputPanel.js
@@ -4,9 +4,15 @@ import FormLongLat from './Forms/FormLongLat';
 import FormCity from './Forms/FormCity';
 
 function InputPanel({formToShow, onIdGet}){
-    
+    if (formToShow === '') {
+        return (
+            <div className="inputPanel">
+                <p>Click on a button from the left to start searching</p>
+            </div>
+        );
+    }
     return (
-        <div>
+        <div className="inputPanel">
             {formToShow === 'id' && <FormId onGet = {onIdGet} />}
             {formToShow === 'location' && <FormLongLat />}
             {formToShow === 'cityPref' && <FormCity />}
