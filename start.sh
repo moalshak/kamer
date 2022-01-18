e2056bef67cf2a5a2001cb2a93ed22bd8ebdbb9d
Mohammad <mo.alshakoush@gmail.com>
2022-01-18 18:01:43 +0100

    updating scripts

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+
+# run env
+source ../env/bin/activate
+
 # cd into the folder
 cd kamer/react-frontend
 
-# start the server
-npm start
 
+# this fixed a no module found
+rm -rf node_modules
+npm install
 
 # build the website
 npm run build
+
+# start the server (REACT)
+python3 manage.py collectstatic --noinput
+python3 manage.py runserver
